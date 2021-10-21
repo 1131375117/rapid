fe8d0a6bc6c7f4ee73851eb46087f0acfe53b6a9
付华 <fuhua@hua-cloud.com.cn>
2021-10-21 16:19:09 +0800

    税收优惠新增接口编写

--- src/main/java/cn/huacloud/taxpreference/services/producer/impl/TaxPreferenceServiceImpl.java
+++ src/main/java/cn/huacloud/taxpreference/services/producer/impl/TaxPreferenceServiceImpl.java
@@ -9,15 +9,11 @@ import cn.huacloud.taxpreference.services.producer.entity.dtos.TaxPreferenceDTO;
 import cn.huacloud.taxpreference.services.producer.mapper.SubmitConditionMapper;
 import cn.huacloud.taxpreference.services.producer.mapper.TaxPreferenceMapper;
 import cn.huacloud.taxpreference.services.producer.mapper.TaxPreferencePoliciesMapper;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
