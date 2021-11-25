a268148db04237159fb56b246796631840fed486
付华 <fuhua@hua-cloud.com.cn>
2021-11-25 16:16:10 +0800

    新增税收优惠详情搜索接口

--- src/main/java/cn/huacloud/taxpreference/services/consumer/OtherDocSearchService.java
+++ src/main/java/cn/huacloud/taxpreference/services/consumer/OtherDocSearchService.java
@@ -7,6 +7,11 @@ import cn.huacloud.taxpreference.services.consumer.entity.vos.OtherDocVO;
  * @author fuhua
  **/
 public interface OtherDocSearchService extends SearchService<OtherDocDTO,OtherDocVO>{
-
+    /**
+     * 税收优惠详情
+     * @param id
+     * @return
+     * @throws Exception
+     */
     OtherDocVO getTaxOtherDocDetails(Long id) throws Exception;
 }
