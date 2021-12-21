5994104e0768c4d9d6767121e337e59370323165
付华 <fuhua@hua-cloud.com.cn>
2021-12-21 11:15:41 +0800

    新增纳税人资格类型

--- src/main/java/cn/huacloud/taxpreference/common/constants/ConditionName.java
+++ src/main/java/cn/huacloud/taxpreference/common/constants/ConditionName.java
@@ -8,4 +8,5 @@ public interface ConditionName {
     String ANNUAL_PROFIT= "是否获利或者获利年度";
     String TAXPAYER_CREDIT_RATINGS= "纳税信用等级";
     String RESOURCE_TYPE= "资源类型";
+    String TYPES_TAXPAYER_ELIGIBILITY ="纳税人资格类型";
 }
