25b9f1e5655cd0ad6c4fc7186e3407f0251c24c2
zqk <zqkzuishuai@sina.com>
2021-12-30 11:36:45 +0800

    feat: 添加热门问答附件

--- src/main/java/cn/huacloud/taxpreference/sync/spider/entity/dtos/FrequentlyAskedQuestionCombineDTO.java
+++ src/main/java/cn/huacloud/taxpreference/sync/spider/entity/dtos/FrequentlyAskedQuestionCombineDTO.java
@@ -1,6 +1,8 @@
 package cn.huacloud.taxpreference.sync.spider.entity.dtos;
 
+import cn.huacloud.taxpreference.services.common.entity.dos.AttachmentDO;
 import cn.huacloud.taxpreference.services.producer.entity.dos.FrequentlyAskedQuestionDO;
+import java.util.List;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -17,5 +19,10 @@ public class FrequentlyAskedQuestionCombineDTO {
      * ax_preference_dev库 热门回答 对象
      */
     private FrequentlyAskedQuestionDO frequentlyAskedQuestionDO;
+
+    /**
+     * ax_preference_dev库  附件对象list
+     */
+    private List<AttachmentDO> attachmentDOList;
 
 }
