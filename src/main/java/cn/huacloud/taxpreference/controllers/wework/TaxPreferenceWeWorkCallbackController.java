e07e281f26fbaa557c072600a59ffc32af443ecf
wangkh <wangkh@hua-cloud.com.cn>
2022-03-10 20:36:30 +0800

    回调接口类修改

--- src/main/java/cn/huacloud/taxpreference/controllers/wework/TaxPreferenceWeWorkCallbackController.java
+++ src/main/java/cn/huacloud/taxpreference/controllers/wework/TaxPreferenceWeWorkCallbackController.java
@@ -1,49 +1,59 @@
 package cn.huacloud.taxpreference.controllers.wework;
 
+import cn.huacloud.taxpreference.services.wework.CallbackService;
+import cn.huacloud.taxpreference.services.wework.entity.model.CallbackQuery;
+import cn.huacloud.taxpreference.services.wework.support.WeWorkConstants;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author wangkh
  */
 @Slf4j
-@Api(tags = "企业微信回调")
+@Api(tags = "企业微信税小秘回调")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 @RestController
 public class TaxPreferenceWeWorkCallbackController {
 
-    /*private final TaxPreferenceWWXService taxPreferenceWWXService;
+    private final String appName = WeWorkConstants.APP_NAME_TAX_PREFERENCE;
+
+    @Autowired
+    private CallbackService callbackService;
 
     @ApiOperation("验证URL有效性")
     @GetMapping("/wwx/callback/*")
-    public ResponseEntity<String> verifyURL(HttpServletRequest request, CallbackQueryDTO queryDTO) throws Exception {
+    public ResponseEntity<String> verifyURL(HttpServletRequest request, CallbackQuery query) throws Exception {
         log.info("验证URL有效性回调，URL：{}", request.getRequestURL());
-        String result = taxPreferenceWWXService.verifyURL(queryDTO);
+        String result = callbackService.verifyUrl(appName, query);
         return ResponseEntity.ok().body(result);
     }
 
     @ApiOperation("安装完成回调、注册回调")
     @PostMapping("/wwx/callback/install")
-    public ResponseEntity<String> installCallback(CallbackQueryDTO queryDTO, @RequestBody String bodyStr) throws Exception {
-        String result = taxPreferenceWWXService.installCallback(queryDTO, bodyStr);
+    public ResponseEntity<String> installCallback(CallbackQuery query, @RequestBody String postData) throws Exception {
+        String result = callbackService.installCallback(appName, query, postData);
         return ResponseEntity.ok().body(result);
     }
 
     @ApiOperation("数据回调")
     @PostMapping("/wwx/callback/data")
-    public ResponseEntity<String> dataCallback(CallbackQueryDTO queryDTO, @RequestBody String bodyStr) throws Exception {
-        String result = taxPreferenceWWXService.dataCallback(queryDTO, bodyStr);
+    public ResponseEntity<String> dataCallback(CallbackQuery query, @RequestBody String postData) throws Exception {
+        String result = callbackService.dataCallback(appName, query, postData);
         return ResponseEntity.ok().body(result);
     }
 
     @ApiOperation("指令回调")
     @PostMapping("/wwx/callback/instruct")
-    public ResponseEntity<String> instructCallback(CallbackQueryDTO queryDTO, @RequestBody String bodyStr) throws Exception {
-        String result = taxPreferenceWWXService.instructCallback(queryDTO, bodyStr);
+    public ResponseEntity<String> instructCallback(CallbackQuery query, @RequestBody String postData) throws Exception {
+        String result = callbackService.instructCallback(appName, query, postData);
         return ResponseEntity.ok().body(result);
-    }*/
+    }
 }
