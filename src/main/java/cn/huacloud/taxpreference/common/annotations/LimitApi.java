915df2c829d282ccc80e36c671f78cb000fae3f0
付华 <fuhua@hua-cloud.com.cn>
2022-03-21 15:06:27 +0800

    删减代码

--- src/main/java/cn/huacloud/taxpreference/common/annotations/LimitApi.java
+++ src/main/java/cn/huacloud/taxpreference/common/annotations/LimitApi.java
@@ -1,7 +1,5 @@
 package cn.huacloud.taxpreference.common.annotations;
 
-import cn.huacloud.taxpreference.common.enums.LimitType;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -17,16 +15,4 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LimitApi {
 
-    /**
-     * 限制类型
-     */
-    LimitType limitType() default LimitType.LEAKY_BUCKET;
-
-    /**
-     * 每秒可处理请求数,exec_count_per_second
-     */
-    int execCountPerSecond() default 800;
-
-    /** 限流过期时间，默认1秒，即每durationSeconds秒允许多少请求（当limit_type=1时有效）, 数据库字段：durationSeconds */
-    int durationSeconds() default 1;
 }
