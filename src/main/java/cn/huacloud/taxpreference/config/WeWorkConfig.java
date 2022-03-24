684bd3f7bfb018a9cbbd47cf4f1b814f65e7e298
wangkh <wangkh@hua-cloud.com.cn>
2022-03-24 11:29:33 +0800

    添加微信接口http代理配置

--- src/main/java/cn/huacloud/taxpreference/config/WeWorkConfig.java
+++ src/main/java/cn/huacloud/taxpreference/config/WeWorkConfig.java
@@ -3,6 +3,7 @@ package cn.huacloud.taxpreference.config;
 import cn.huacloud.taxpreference.services.wework.entity.model.AppConfig;
 import cn.huacloud.taxpreference.services.wework.support.WeWorkConstants;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +26,12 @@ public class WeWorkConfig {
      */
     private AppConfig taxTool;
 
+    /**
+     * http代理配置
+     */
+    private ProxyConfig proxy;
+
+
     public AppConfig getAppConfigByAppName(String appName) {
         // TODO 新加入的应用需要在这里配置
         if (WeWorkConstants.APP_NAME_TAX_PREFERENCE.equals(appName)) {
@@ -43,5 +50,18 @@ public class WeWorkConfig {
     public String getAppNameByUri(String requestURI) {
         // TODO 新加入的应用需要在这里配置
         return WeWorkConstants.APP_NAME_TAX_PREFERENCE;
+    }
+
+    @Data
+    public static class ProxyConfig {
+        private Boolean enable = false;
+        private String host;
+        private Integer port;
+        private String username;
+        private String password;
+
+        public boolean useAuth() {
+            return StringUtils.isNotBlank(username) && StringUtils.isNotBlank(password);
+        }
     }
 }
--- src/main/java/cn/huacloud/taxpreference/services/wework/client/config/FeignClientConfig.java
+++ src/main/java/cn/huacloud/taxpreference/services/wework/client/config/FeignClientConfig.java
@@ -1,18 +1,15 @@
 package cn.huacloud.taxpreference.services.wework.client.config;
 
+import cn.huacloud.taxpreference.config.WeWorkConfig;
 import feign.Capability;
 import feign.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSocketFactory;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * feign client http 代理配置
@@ -20,19 +17,24 @@ import java.util.stream.Collectors;
  */
 @Configuration
 public class FeignClientConfig {
-    public static String proxyHost = "1.117.164.184";
-    public static Integer proxyPort = 32000;
 
     @Bean
-    public Client proxyClient(Environment environment) {
-        Set<String> profiles = Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toSet());
-        if (!profiles.contains("dev")) {
+    public Client proxyClient(WeWorkConfig weWorkConfig) {
+
+        WeWorkConfig.ProxyConfig proxyConfig = weWorkConfig.getProxy();
+        if (!proxyConfig.getEnable()) {
+            // 不启动http代理
             return new Client.Default(null, null);
         }
-        // 只有在开发环境才启动http代理，上到生产环境ip本来就是有效的。
-        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
-        ProxyClient proxyClient = new ProxyClient(null, null, proxy);
-        return proxyClient;
+
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyConfig.getHost(), proxyConfig.getPort()));
+
+        if (proxyConfig.useAuth()) {
+            return new ProxyClient(null, null, proxy, proxyConfig.getUsername(), proxyConfig.getPassword());
+        } else {
+
+            return new ProxyClient(null, null, proxy);
+        }
     }
 
     /**
